42fba03a46caa02d19ee5e6d34a354f0078795bb
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-25 17:30:24 +0200

    Init

---
+++ src/main/java/ua/kovalchuk/aerospikeclients/AerospikeClientsApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.aerospikeclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerospikeClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AerospikeClientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/aerospikeclients/AerospikeClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.aerospikeclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerospikeClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
